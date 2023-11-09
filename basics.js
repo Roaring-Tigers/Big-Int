18c05a255b162604b4d7850f0ff4078e85ca97fa
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-09 11:49:47 +0530

    basics

---
+++ basics.js
@@ -0,0 +1,29 @@
+// let a = BigInt("100000000000000000000000000000000000000000000000000")
+
+
+// console.log(a)
+
+// BigInit
+
+// let x = 10n 
+// let y = 20n
+
+// let z = x+y 
+// console.log(z)
+
+// let div = y/x 
+// console.log(div)
+
+
+// let a = 10n  // Number(10n) 
+// let b = 5  //BigInt(5) 
+// console.log(a+b)
+
+
+
+// console.log(1n === 1) 
+
+
+// let x = BigInt(100 + "n)
+
+console.log(typeof x)
